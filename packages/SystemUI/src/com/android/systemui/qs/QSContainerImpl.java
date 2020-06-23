StatiXOS/android_frameworks_base

base: QS header opacity
Change-Id: Ieabb916f065a91be0cacbc88fe0028d0ffc19f56

packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
@@ -21,8 +21,14 @@ import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.database.ContentObserver;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -34,6 +40,7 @@ import com.android.systemui.qs.customize.QSCustomizer;
  */
 public class QSContainerImpl extends FrameLayout {
 
+    public final String TAG = "QSContainerImpl";
     private final Point mSizePoint = new Point();
 
     private int mHeightOverride = -1;
@@ -51,8 +58,13 @@ public class QSContainerImpl extends FrameLayout {
     private int mSideMargins;
     private boolean mQsDisabled;
 
+    private Drawable mQsHeaderBg;
+
     public QSContainerImpl(Context context, AttributeSet attrs) {
         super(context, attrs);
+        Handler mHandler = new Handler();
+        SettingsObserver settingsObserver = new SettingsObserver(mHandler);
+        settingsObserver.observe();
     }
 
     @Override
@@ -67,6 +79,8 @@ public class QSContainerImpl extends FrameLayout {
         mStatusBarBackground = findViewById(R.id.quick_settings_status_bar_background);
         mBackgroundGradient = findViewById(R.id.quick_settings_gradient_view);
         mSideMargins = getResources().getDimensionPixelSize(R.dimen.notification_side_paddings);
+        mQsHeaderBg = getContext().getDrawable(R.drawable.qs_header_background);
+        updateSettings();
 
         setImportantForAccessibility(IMPORTANT_FOR_ACCESSIBILITY_NO);
         setMargins();
@@ -79,6 +93,37 @@ public class QSContainerImpl extends FrameLayout {
         updateResources();
         mSizePoint.set(0, 0); // Will be retrieved on next measure pass.
     }
+
+    private void updateSettings() {
+         int mQsHeaderBgAlpha = Settings.System.getIntForUser(getContext().getContentResolver(),
+                 Settings.System.QS_HEADER_BG_ALPHA, 255,
+                 UserHandle.USER_CURRENT);
+         Log.d(TAG, Integer.toString(mQsHeaderBgAlpha));
+         if (mQsHeaderBgAlpha < 255 ) {
+             mHeader.setVisibility(View.INVISIBLE);
+             mQsHeaderBg.setAlpha(mQsHeaderBgAlpha);
+             setBackground(mQsHeaderBg);
+         } else {
+             mHeader.setVisibility(View.VISIBLE);
+         }
+     }
+
+    private class SettingsObserver extends ContentObserver {
+         SettingsObserver(Handler handler) {
+             super(handler);
+         }
+
+         void observe() {
+             getContext().getContentResolver().registerContentObserver(Settings.System
+                             .getUriFor(Settings.System.QS_HEADER_BG_ALPHA), false,
+                     this, UserHandle.USER_ALL);
+         }
+
+         @Override
+         public void onChange(boolean selfChange) {
+             updateSettings();
+         }
+     }
 
     @Override
     public boolean performClick() {

4PERTURE/android_frameworks_base-4

packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
@@ -21,7 +21,12 @@ import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
 
 import android.content.Context;
 import android.content.res.Configuration;
+import android.database.ContentObserver;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -51,8 +56,13 @@ public class QSContainerImpl extends FrameLayout {
     private int mSideMargins;
     private boolean mQsDisabled;
 
+    private Drawable mQsHeaderBg;
+
     public QSContainerImpl(Context context, AttributeSet attrs) {
         super(context, attrs);
+        Handler mHandler = new Handler();
+        SettingsObserver settingsObserver = new SettingsObserver(mHandler);
+        settingsObserver.observe();
     }
 
     @Override
@@ -67,6 +77,8 @@ public class QSContainerImpl extends FrameLayout {
         mStatusBarBackground = findViewById(R.id.quick_settings_status_bar_background);
         mBackgroundGradient = findViewById(R.id.quick_settings_gradient_view);
         mSideMargins = getResources().getDimensionPixelSize(R.dimen.notification_side_paddings);
+        mQsHeaderBg = getContext().getDrawable(R.drawable.qs_header_background);
+        updateSettings();
 
         setImportantForAccessibility(IMPORTANT_FOR_ACCESSIBILITY_NO);
         setMargins();
@@ -79,6 +91,35 @@ public class QSContainerImpl extends FrameLayout {
         updateResources();
         mSizePoint.set(0, 0); // Will be retrieved on next measure pass.
     }
+
+    private void updateSettings() {
+         int mQsHeaderBgAlpha = Settings.System.getIntForUser(getContext().getContentResolver(),
+                 Settings.System.QS_HEADER_BG_ALPHA, 255,
+                 UserHandle.USER_CURRENT);
+         if (mQsHeaderBgAlpha < 255) {
+             mQsHeaderBg.setAlpha(mQsHeaderBgAlpha);
+             mStatusBarBackground.setBackground(mQsHeaderBg);
+         } else {
+             mStatusBarBackground.setVisibility(View.VISIBLE);
+         }
+     }
+
+    private class SettingsObserver extends ContentObserver {
+         SettingsObserver(Handler handler) {
+             super(handler);
+         }
+
+         void observe() {
+             getContext().getContentResolver().registerContentObserver(Settings.System
+                             .getUriFor(Settings.System.QS_HEADER_BG_ALPHA), false,
+                     this, UserHandle.USER_ALL);
+         }
+
+         @Override
+         public void onChange(boolean selfChange) {
+             updateSettings();
+         }
+     }
 
     @Override
     public boolean performClick() {