ArrowOS/android_frameworks_base

TrafficSB: Hide arrows in statusbar traffic indicators [1/2]
arrow-edits:
adapt code to our dynamic arrows method

Change-Id: Ie50dbe3f54495d4b5c0c0a51969df595de7a1386

core/java/android/provider/Settings.java
@@ -4789,6 +4789,12 @@ public final class Settings {
          */
         public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
 
+	/**
+         * Whether to disable showing arrows in statusbar network traffic indicators
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_HIDEARROW_SB = "network_traffic_hidearrow_sb";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.

4PERTURE/android_frameworks_base-4

Signed-off-by: Sebita <kjjjnob.seba@gmail.com>

core/java/android/provider/Settings.java
@@ -4839,6 +4839,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
+         * Whether to disable showing arrows in statusbar network traffic indicators
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *